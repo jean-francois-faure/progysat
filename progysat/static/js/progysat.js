4165cb28cfcaad92c384140c375d9f95a18f0b0a
Maxime Bellec <maxime@telescoop.fr>
2023-06-12 12:14:14 +0200

    feat: new navbar logo

--- progysat/static/js/progysat.js
+++ progysat/static/js/progysat.js
@@ -21,3 +21,24 @@ function toggleLanguageDropdown() {
         el.classList.add('is-active');
     }
 }
+
+// make Bulma navbar work
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+      console.log("### click on navbar1");
+      // Get the target from the "data-target" attribute
+      var target = el.dataset.target;
+      var $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+});
